5e76c2048c4f1ce09edf2eca410f0eff7be3c64e
paolov85 <pvicaro@gmail.com>
2023-07-05 18:38:50 +0200

    navbar bugfix

--- assets/script/navbar.js
+++ assets/script/navbar.js
@@ -18,11 +18,8 @@ window.onload = function() {
       }
 
   window.addEventListener("resize", function() {
-    // Controlla la larghezza della finestra
     if (window.innerWidth > 768) {
-      // Itera su tutti gli elementi
       for (var i = 0; i < mobileNav.length; i++) {
-        // Rimuovi l'attributo HTML dall'elemento corrente
         mobileNav[i].removeAttribute("data-bs-toggle");
         mobileNav[i].setAttribute("style", "display: inline-block; width: 100%;");
         mobileNavLink[i].removeAttribute("style");
@@ -31,11 +28,8 @@ window.onload = function() {
   });
 
   window.addEventListener("resize", function() {
-    // Controlla la larghezza della finestra
     if (window.innerWidth < 768) {
-      // Itera su tutti gli elementi
       for (var i = 0; i < mobileNav.length; i++) {
-        // Rimuovi l'attributo HTML dall'elemento corrente
         mobileNav[i].setAttribute("data-bs-toggle", "collapse");
         mobileNav[i].setAttribute("style", "display: inline-block; width: 100%; height: 40px");
         mobileNavLink[i].setAttribute("style", "padding: 0;");
