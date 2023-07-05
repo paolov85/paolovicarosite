8c2e36358bffcd803d48e2000d0b8385c0daed2e
paolov85 <pvicaro@gmail.com>
2023-07-05 17:48:58 +0200

    js improvements

--- assets/script/navbar.js
+++ assets/script/navbar.js
@@ -1,30 +1,47 @@
-// Ottieni tutti gli elementi con la classe specifica
-var mobileNav = document.getElementsByClassName("mobileNav");
-var mobileNavLink = document.getElementsByClassName("mobileNavLink");
+window.onload = function() {
+  console.log("La pagina è stata completamente caricata.");
 
-// Aggiungi un listener per il cambiamento di dimensione della finestra
-window.addEventListener("resize", function() {
-  // Controlla la larghezza della finestra
+  var mobileNav = document.getElementsByClassName("mobileNav");
+  var mobileNavLink = document.getElementsByClassName("mobileNavLink");
+
   if (window.innerWidth > 768) {
-    // Itera su tutti gli elementi
-    for (var i = 0; i < mobileNav.length; i++) {
-      // Rimuovi l'attributo HTML dall'elemento corrente
-      mobileNav[i].removeAttribute("data-bs-toggle");
-      mobileNav[i].setAttribute("style", "display: inline-block; width: 100%;");
-      mobileNavLink[i].removeAttribute("style");
-    }
-  }
-});
+      for (var i = 0; i < mobileNav.length; i++) {
+        mobileNav[i].removeAttribute("data-bs-toggle");
+        mobileNav[i].setAttribute("style", "display: inline-block; width: 100%;");
+        mobileNavLink[i].removeAttribute("style");
+      }
+    } else {
+        for (var i = 0; i < mobileNav.length; i++) {
+          mobileNav[i].setAttribute("data-bs-toggle", "collapse");
+          mobileNav[i].setAttribute("style", "display: inline-block; width: 100%; height: 40px");
+          mobileNavLink[i].setAttribute("style", "padding: 0;");
+        }
+      }
 
-window.addEventListener("resize", function() {
-  // Controlla la larghezza della finestra
-  if (window.innerWidth < 768) {
-    // Itera su tutti gli elementi
-    for (var i = 0; i < mobileNav.length; i++) {
-      // Rimuovi l'attributo HTML dall'elemento corrente
-      mobileNav[i].setAttribute("data-bs-toggle", "collapse");
-      mobileNav[i].setAttribute("style", "display: inline-block; width: 100%; height: 40px");
-      mobileNavLink[i].setAttribute("style", "padding: 0;");
+  window.addEventListener("resize", function() {
+    // Controlla la larghezza della finestra
+    if (window.innerWidth > 768) {
+      // Itera su tutti gli elementi
+      for (var i = 0; i < mobileNav.length; i++) {
+        // Rimuovi l'attributo HTML dall'elemento corrente
+        mobileNav[i].removeAttribute("data-bs-toggle");
+        mobileNav[i].setAttribute("style", "display: inline-block; width: 100%;");
+        mobileNavLink[i].removeAttribute("style");
+      }
     }
-  }
-});
+  });
+
+  window.addEventListener("resize", function() {
+    // Controlla la larghezza della finestra
+    if (window.innerWidth < 768) {
+      // Itera su tutti gli elementi
+      for (var i = 0; i < mobileNav.length; i++) {
+        // Rimuovi l'attributo HTML dall'elemento corrente
+        mobileNav[i].setAttribute("data-bs-toggle", "collapse");
+        mobileNav[i].setAttribute("style", "display: inline-block; width: 100%; height: 40px");
+        mobileNavLink[i].setAttribute("style", "padding: 0;");
+      }
+    }
+  });
+
+};
