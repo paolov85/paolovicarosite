aacc43536b59d0a1ccd044db71773c2340d2fa5c
paolov85 <pvicaro@gmail.com>
2023-07-06 09:39:52 +0200

    js alternative test

--- assets/script/navbar.js
+++ assets/script/navbar.js
@@ -1,40 +1,10 @@
-window.onload = function() {
- 
-  var mobileNav = document.getElementsByClassName("mobileNav");
-  var mobileNavLink = document.getElementsByClassName("mobileNavLink");
+// Ottieni tutti gli elementi con la classe specifica
+var elementi = document.getElementsByClassName("navbar-collapse");
 
-  if (window.innerWidth > 768) {
-      for (var i = 0; i < mobileNav.length; i++) {
-        mobileNav[i].removeAttribute("data-bs-toggle");
-        mobileNav[i].setAttribute("style", "display: inline-block; width: 100%;");
-        mobileNavLink[i].removeAttribute("style");
-      }
-    } else {
-        for (var i = 0; i < mobileNav.length; i++) {
-          mobileNav[i].setAttribute("data-bs-toggle", "collapse");
-          mobileNav[i].setAttribute("style", "display: inline-block; width: 100%; height: 40px");
-          mobileNavLink[i].setAttribute("style", "padding: 0;");
-        }
-      }
-
-  window.addEventListener("resize", function() {
-    if (window.innerWidth > 768) {
-      for (var i = 0; i < mobileNav.length; i++) {
-        mobileNav[i].removeAttribute("data-bs-toggle");
-        mobileNav[i].setAttribute("style", "display: inline-block; width: 100%;");
-        mobileNavLink[i].removeAttribute("style");
-      }
-    }
+// Aggiungi un event listener per l'evento di click a ciascun elemento
+for (var i = 0; i < elementi.length; i++) {
+  elementi[i].addEventListener("click", function() {
+    // Rimuovi la classe dall'elemento cliccato
+    this.classList.remove("show");
   });
-
-  window.addEventListener("resize", function() {
-    if (window.innerWidth < 768) {
-      for (var i = 0; i < mobileNav.length; i++) {
-        mobileNav[i].setAttribute("data-bs-toggle", "collapse");
-        mobileNav[i].setAttribute("style", "display: inline-block; width: 100%; height: 40px");
-        mobileNavLink[i].setAttribute("style", "padding: 0;");
-      }
-    }
-  });
-
-};
+}
